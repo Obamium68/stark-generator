e04fc969b486afdb46f81401de29fa92182d4d1d
Andrea <andrea.f.matera@gmail.com>
2025-09-27 12:30:02 +0200

    Added cors origin call restrictive policy

--- run.py
+++ run.py
@@ -4,9 +4,12 @@ Application entry point
 import os
 from app import create_app
 from app.config import config
+from flask_cors import CORS
+
 
 config_name = os.environ.get('FLASK_ENV', 'development')
 app = create_app(config[config_name])
+CORS(app, origins=os.environ.get('CORS_ORIGIN'))
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
